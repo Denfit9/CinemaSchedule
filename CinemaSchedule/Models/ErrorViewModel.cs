e3b4550261ef3870642b034d40d1f6a4cc104220
Denfit9 <d03062002@gmail.com>
2024-05-07 15:57:53 +0300

    Initial commit.

---
+++ CinemaSchedule/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CinemaSchedule.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
